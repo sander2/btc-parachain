97f1f71d65872ebc6ca77d45137da01ab84a1b85
Sander Bosma <sanderbosma@gmail.com>
2021-11-15 15:15:21 +0100

    chore: disable slow test

--- crates/vault-registry/src/tests.rs
+++ crates/vault-registry/src/tests.rs
@@ -862,6 +862,7 @@ fn calculate_max_wrapped_from_collateral_for_threshold_succeeds() {
 }
 
 #[test]
+#[ignore] // slow test, disabled so that tarpaulin doesn't time out
 fn test_threshold_equivalent_to_legacy_calculation() {
     /// old version
     fn legacy_calculate_max_wrapped_from_collateral_for_threshold(
@@ -901,6 +902,7 @@ fn test_threshold_equivalent_to_legacy_calculation() {
 }
 
 #[test]
+#[ignore] // slow test, disabled so that tarpaulin doesn't time out
 fn test_get_required_collateral_threshold_equivalent_to_legacy_calculation_() {
     // old version
     fn legacy_get_required_collateral_for_wrapped_with_threshold(
